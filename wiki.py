e95d010647f382abee9513ee367009eb8ad23174
Geidel Xavier <geidelxavier@google.com>
2021-05-25 21:31:26 +0000

    getting started

--- wiki.py
+++ wiki.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask 
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Hello, World!\n"
+    return handle_request()
+
+@app.route("/about")
+def handle_request(name="wiki",content="stuff"):
+    # TODO: load the desired page content
+    return render_template(
+        "main.html",
+        page_name=name,
+        page_content=content,
+    )
