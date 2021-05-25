6db57ed8233c9cb74a494239b413b253c33abc0e
Johnny Castillo <johnnycastillo@google.com>
2021-05-25 23:47:43 +0000

    implemented requirement 1,and 2

--- wiki.py
+++ wiki.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return handle_request()
+    #return handle_request()
+    return front_page_request()
+
 
 @app.route("/about")
 def handle_request(name="wiki",content="stuff"):
@@ -13,5 +15,34 @@ def handle_request(name="wiki",content="stuff"):
     return render_template(
         "main.html",
         page_name=name,
+        page_content=content,
+    )
+
+
+@app.route("/view")
+def front_page_request(wiki_name="Fedora's wiki"):
+    with open("pages/FrontPage.txt", "r") as file:
+        content = file.read()
+
+    # TODO: load the desired page content
+    return render_template(
+        "main.html",
+        page_name=wiki_name,
+        page_content=content,
+    )
+
+#@app.route('/post/<int:post_id>')
+
+@app.route("/view/<page_name>")
+def page_request(page_name):
+    try:
+        with open("pages/" + str(page_name), "r") as file:
+            content = file.read()
+    except:
+        raise FileNotFoundError 
+    # TODO: load the desired page content
+    return render_template(
+        "main.html",
+        page_name=page_name,
         page_content=content,
     )
