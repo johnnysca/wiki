b9134ceb6fd41674a5c027c25bfd11c94cb7bf23
Geidel Xavier <geidelxavier@google.com>
2021-05-27 00:48:50 +0000

    changed how page request reads files, added PageName.txt and additional test case

--- wiki.py
+++ wiki.py
@@ -8,7 +8,7 @@ def main():
     #return handle_request()
     return front_page_request()
 
-
+#Example function, to be changed or deleted later
 @app.route("/about")
 def handle_request(name="wiki",content="stuff"):
     # TODO: load the desired page content
@@ -24,23 +24,22 @@ def front_page_request(wiki_name="Fedora's wiki"):
     with open("pages/FrontPage.txt", "r") as file:
         content = file.read()
 
-    # TODO: load the desired page content
+    # Load the desired page content
     return render_template(
         "main.html",
         page_name=wiki_name,
         page_content=content,
     )
 
-#@app.route('/post/<int:post_id>')
-
 @app.route("/view/<path:page_name>")
 def page_request(page_name):
     try:
-        with open("pages/" + str(page_name), "r") as file:
+        with open("pages/" + str(page_name) + ".txt", "r") as file:
             content = file.read()
     except:
         raise FileNotFoundError 
-    # TODO: load the desired page content
+
+    # Load the desired page content
     return render_template(
         "main.html",
         page_name=page_name,
--- wiki_test.py
+++ wiki_test.py
@@ -1,12 +1,6 @@
 import pathlib
-
 import pytest
-
-
-
 import wiki
-
-
 
 
 @pytest.fixture
@@ -16,24 +10,25 @@ def client():
     with wiki.app.test_client() as client:
         yield client
 
-
 def test_import():
     assert wiki is not None
 
-#TODO: update hello world to something consistent for our test directory
+#TODO: update test to something consistent for our test directory
 def test_homepage(client):
     resp = client.get("/")
     assert resp.status_code == 200
-    #assert b"Hello, World!\n" in resp.data
+    assert resp.data is not None
+
+def test_page_name(client):
+    resp = client.get("/view/PageName")
+    assert resp.status_code == 200
+    assert resp.data is not None
 
 def test_empty_page_name(client):
     with pytest.raises(FileNotFoundError) as e:
         resp = client.get("/view/<TMNT>")
 
 def test_view_page_name(client):
- 
-    resp = client.get("/view/test_pages/name_test.txt")
-
+    resp = client.get("/view/test_pages/name_test")
+    assert resp.status_code == 200
     assert b"Geidel\nKevin\nJohnny" in resp.data
-
-
