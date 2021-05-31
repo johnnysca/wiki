de7d2b3ffa4c4bf60de83318931b39e11c59b15b
Kevin Moreno <kjmtxep@google.com>
2021-05-31 17:24:44 +0000

    added Marko dependency: a markdown lang parser

---
+++ format.py
@@ -0,0 +1,5 @@
+import marko 
+
+print(marko.convert("# Hello"))
+print(marko.convert("El rancho de geidel"))
+print(marko.convert("[This is the ranch](https://images.app.goo.gl/jnYh7jueoT5iT1PC9)"))
