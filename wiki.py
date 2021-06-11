2549fc4a80cbd7f9e65f4d6e3d54e0ff4b4787fa
Kelvin Estrada <kelvinestrada@google.com>
2021-06-11 21:01:36 +0000

    Added links to edit page and view page history.

--- wiki.py
+++ wiki.py
@@ -66,6 +66,9 @@ def page_request(page_name: str) -> Tuple[str, int]:
 
     with open("templates/" + str(page_name) + ".html", "w") as file:
         file.write(format.formatter(content))
+        if str(page_name) != 'FrontPage':
+            file.write("<a href=" + "/edit/"+str(page_name)+">Edit this page here.</a><br>")
+            file.write("<a href=" + "/history/"+str(page_name)+">View page history.</a><br>")
 
     # Load the desired page content
     return render_template(page_name + ".html"), 200
